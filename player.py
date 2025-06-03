74826972af2f0a807f401ed06446a8ff776760ad
draganovdimitar2 <draganovdimitar2@gmail.com>
2025-06-03 12:00:00 +0300

    feat: add Player class with basic betting and balance tracking

---
+++ player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self):
+        self.balance = 1000.00
+        self.bet_size = 10.00
+        self.last_payout = 0.00
+        self.total_won = 0.00
+        self.total_wager = 0.00
+        self.lines = 1  # desired lines
+        self.player_data = {}
+
+
