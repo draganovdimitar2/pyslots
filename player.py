6b9830aaa3abcb5b72fb1c65b9e229569d82ee2e
draganovdimitar2 <draganovdimitar2@gmail.com>
2025-06-04 12:00:00 +0300

    feat: add get_data and place_bet methods to Player class

--- player.py
+++ player.py
@@ -8,4 +8,15 @@ class Player:
         self.lines = 1  # desired lines
         self.player_data = {}
 
+    def get_data(self):
+        self.player_data['balance'] = f"{self.balance:.2f}"
+        self.player_data['bet_size'] = f"{self.bet_size:.2f}"
+        self.player_data['last_payout'] = f"{self.last_payout:.2f}" if self.last_payout else "N/A"
+        self.player_data['total_won'] = f"{self.total_won:.2f}"
+        self.player_data['total_wager'] = f"{self.total_wager:.2f}"
+        return self.player_data
 
+    def place_bet(self):
+        bet = self.bet_size
+        self.balance -= bet
+        self.total_wager += bet
